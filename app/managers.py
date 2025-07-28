7bd093ad87a87d250d3d6f4d9c9a5eb5e0d31ab5
yarik <super.racing@ukr.net>
2025-07-28 14:47:16 +0300

    created manager for class Actor

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,44 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self._db_name = db_name
+        self._table_name = table_name
+        self._connector = sqlite3.connect(self._db_name)
+
+    def create(self, first_name: str, last_name: str) -> None:
+        self._connector.execute(
+            f"INSERT INTO {self._table_name} (first_name, last_name) VALUES (?, ?);",
+            (first_name, last_name)
+        )
+        self._connector.commit()
+
+    def all(self) -> list:
+        cursor = self._connector.execute(
+            "SELECT * "
+            f"FROM {self._table_name};"
+        )
+        return [
+            Actor(*row) for row in cursor
+        ]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self._connector.execute(
+            f"UPDATE {self._table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?;",
+            (new_first_name, new_last_name, pk)
+        )
+        self._connector.commit()
+
+    def delete(self, pk: int) -> None:
+        self._connector.execute(
+            f"DELETE FROM {self._table_name} "
+            "WHERE id = ?;",
+            (pk,)
+        )
+        self._connector.commit()
+
+    def __del__(self) -> None:
+        self._connector.close()
