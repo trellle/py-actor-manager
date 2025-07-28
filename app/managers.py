f7af5bd9f9beb2d3f308e7fa8b5488a17ccbd92f
yarik <super.racing@ukr.net>
2025-07-28 14:50:29 +0300

    splited line

--- app/managers.py
+++ app/managers.py
@@ -11,7 +11,8 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connector.execute(
-            f"INSERT INTO {self._table_name} (first_name, last_name) VALUES (?, ?);",
+            f"INSERT INTO {self._table_name} "
+            "(first_name, last_name) VALUES (?, ?);",
             (first_name, last_name)
         )
         self._connector.commit()
